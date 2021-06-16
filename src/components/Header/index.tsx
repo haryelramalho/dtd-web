88567c70097684b9b7d777d72b2c92aa39c5e1f4
Matheus Almeida <mat.almeida@live.com>
2021-06-16 00:15:03 -0300

    feat: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,43 @@
+import { Layout, Button } from 'antd';
+import { LogoutOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+import logoToro from '../../assets/images/logo-toro.png';
+
+import styles from './index.module.css';
+
+type HeaderProps = {
+  isAuthenticated: boolean
+  onLogout?: () => void | Promise<void>
+}
+
+function Header(
+  {
+    isAuthenticated = false,
+    onLogout,
+  }: HeaderProps,
+): JSX.Element {
+  return (
+    <Layout.Header className={styles.header}>
+      <div className={styles.logo_container}>
+        <Link to="/">
+          <img
+            className={styles.logo_image}
+            src={logoToro}
+            alt="Logo Toro Investimentos"
+          />
+        </Link>
+      </div>
+
+      {
+        isAuthenticated && (
+          <Button onClick={onLogout} shape="round" icon={<LogoutOutlined />}>
+            Sair
+          </Button>
+        )
+      }
+    </Layout.Header>
+  );
+}
+
+export default Header;
